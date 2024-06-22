b651f08f163ef15c8e40d47919444bfebb23363c
= <=>
2024-06-22 17:45:57 +0530

    feat: added docker files

--- server/routes/shorten.go
+++ server/routes/shorten.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -42,7 +43,6 @@ func ShortenURL(ctx *fiber.Ctx) error {
 	if err == redis.Nil {
 		_ = rDb.Set(database.Ctx, ctx.IP(), os.Getenv("API_QOUTA"), 30*60*time.Second).Err()
 	} else {
-		value, _ = rDb.Get(database.Ctx, ctx.IP()).Result()
 		valInt, _ := strconv.Atoi(value)
 
 		if valInt < 1 {
@@ -64,7 +64,54 @@ func ShortenURL(ctx *fiber.Ctx) error {
 
 	body.Url = helpers.EnforceHTTP(body.Url)
 
-	rDb.Decr(database.Ctx, ctx.IP())
+	/*
+	*	check if there's custom short url provided by user
+	*	if so, proceed.
+	*	Otherwise, create new custom short url
+	 */
 
-	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"success": "Request proceed successfully"})
+	var id string = body.CustomShort
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, _ = r.Get(database.Ctx, id).Result()
+
+	if value != "" {
+		return ctx.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "Url already in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24 // default expiry of 24 hours
+	}
+
+	err = r.Set(database.Ctx, id, body.Url, body.Expiry*3600*time.Second).Err()
+
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connect to server",
+		})
+	}
+
+	response := res{
+		Url:             body.Url,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRatelimitReset: 30,
+		XRateRemaining:  10,
+	}
+
+	rDb.Decr(database.Ctx, ctx.IP())
+	value, _ = rDb.Get(database.Ctx, ctx.IP()).Result()
+	response.XRateRemaining, _ = strconv.Atoi(value)
+	ttl, _ := rDb.TTL(database.Ctx, ctx.IP()).Result()
+	response.XRatelimitReset = ttl / time.Nanosecond / time.Minute
+
+	return ctx.Status(fiber.StatusOK).JSON(response)
 }
